03509171e27ed56d967ac846e8a6a23c252ca1b0
MaksUr <maksim.urakv@gmail.com>
2017-09-08 16:04:20 +0500

    Add dates functions (not correct)

--- cats/birthday.py
+++ cats/birthday.py
@@ -1,5 +1,5 @@
 # TODO: delete this file
-
+from dateutil.relativedelta import relativedelta
 from dateutil.rrule import rrule, MONTHLY, YEARLY, DAILY
 from datetime import date
 
@@ -16,63 +16,92 @@ def calc_age_uptoday(before_date, later_date):
     :type later_date: date
     """
     result = dict()
-    result[YEARS] = get_calendar_items_count(before_date, YEARLY, later_date)
+    later = later_date
+
+    before = before_date
+    mode = YEARLY
+    result[YEARS] = get_calendar_items_count(before, mode, later)
+
+    mode = MONTHLY
     if later_date.month < before_date.month:
-        # Месяц дня рождения еще не наступил
-        dtstart = get_date(later_date.year-1, before_date.month, before_date.day)
-        until = later_date
-        mode = MONTHLY
-        result[MONTHS] = get_calendar_items_count(dtstart, mode, until)
+        # before = date(later_date.year, before_date.month, before_date.day) - relativedelta(years=1)
+        before = before_date + relativedelta(years=result[YEARS]+1)
+        result[MONTHS] = get_calendar_items_count(before, mode, later)
     elif later_date.month > before_date.month:
-        # Месяц дня рождения прошел
-        dtstart = get_date(later_date.year, before_date.month, before_date.day)
-        until = later_date
-        mode = MONTHLY
-        result[MONTHS] = get_calendar_items_count(dtstart, mode, until)
+        before = date(later_date.year, before_date.month, before_date.day)
+        result[MONTHS] = get_calendar_items_count(before, mode, later)
     else:
-        # Месяц дня рождения
         result[MONTHS] = None
 
+    mode = DAILY
     if later_date.day < before_date.day:
-        dtstart = get_date(later_date.year, before_date.month-1, before_date.day)
-        until = later_date
-        mode = DAILY
-        result[DAYS] = get_calendar_items_count(dtstart, mode, until)
-        if result[MONTHS] is None:
-            # Месяц ДР, до дня ДР
-            dtstart = get_date(later_date.year - 1, before_date.month, before_date.day)
-            until = later_date
-            mode = MONTHLY
-            result[MONTHS] = get_calendar_items_count(dtstart, mode, until)
+        before = date(later_date.year, later_date.month, before_date.day) - relativedelta(months=1)
 
-    elif later_date.day >= before_date.day:
-        dtstart = get_date(later_date.year, later_date.month, before_date.day)
-        until = later_date
-        mode = DAILY
-        result[DAYS] = get_calendar_items_count(dtstart, mode, until)
+        result[DAYS] = get_calendar_items_count(before, mode, later)
         if result[MONTHS] is None:
-            # Месяц ДР, во время и после дня ДР
-            dtstart = get_date(later_date.year, before_date.month, before_date.day)
-            until = later_date
             mode = MONTHLY
-            result[MONTHS] = get_calendar_items_count(dtstart, mode, until)
+            before = date(later_date.year, before_date.month, before_date.day) - relativedelta(years=1)
+            result[MONTHS] = get_calendar_items_count(before, mode, later)
+    elif later_date.day > before_date.day:
+        before = date(later_date.year, later_date.month, before_date.day)
+        result[DAYS] = get_calendar_items_count(before, mode, later)
+        if result[MONTHS] is None:
+            mode = MONTHLY
+            before = date(later_date.year, later_date.month, before_date.day)
+            result[MONTHS] = get_calendar_items_count(before, mode, later)
+    else:
+        before = date(later_date.year, later_date.month, before_date.day)
+        result[DAYS] = get_calendar_items_count(before, mode, later)
+        if result[MONTHS] is None:
+            mode = MONTHLY
+            before = date(later_date.year, later_date.month, before_date.day)
+            result[MONTHS] = get_calendar_items_count(before, mode, later) + 1
+
+
     return result
 
 
+def disclosure_dates(dtstart, rd, dtend):
+    """
+
+    :type rd: relativedelta
+    :type dtend: date
+    :type dtstart: date
+    """
+    if dtstart > dtend:
+        message = 'dtstart={start} is before dtend={end}.'
+        raise ValueError(message.format(start=str(dtstart), end=str(dtend)))
+    ii = 1
+    while True:
+        cdate = dtstart + ii*rd
+        if cdate > dtend:
+            return ii
+        elif cdate == dtend:
+            return ii+1
+        ii += 1
+
+
 def get_calendar_items_count(dtstart, mode, until):
+    if mode == YEARLY:
+        rd = relativedelta(years=1)
+    elif mode == MONTHLY:
+        rd = relativedelta(months=1)
+    else:  #elif mode == DAILY:
+        rd = relativedelta(days=1)
+    res = disclosure_dates(dtstart, rd, until) - 1  # TODO: change disclosure_dates
     # TODO: use relative delta
-    res = rrule(mode, dtstart=dtstart, until=until).count() - 1
+    # res = rrule(mode, dtstart=dtstart, until=until).count() - 1
     return res
 
 
-def get_date(year, month, day):
-    try:
-        res = date(year, month, day)
-    except ValueError as e:
-        if 'day is out of range for month' == e.args[0]:
-            return get_date(year, month, day-1)
-    else:
-        return res
+# def get_date(year, month, day):
+#     try:
+#         res = date(year, month, day)
+#     except ValueError as e:
+#         if 'day is out of range for month' == e.args[0]:
+#             return get_date(year, month, day-1)
+#     else:
+#         return res
 
 
 
--- cats/test_calculate_age_with_up_to_day.py
+++ cats/test_calculate_age_with_up_to_day.py
@@ -8,28 +8,116 @@ from cats.birthday import calc_age_uptoday, YEARS, MONTHS, DAYS
 class TestCalcAgeUpToDay(TestCase):
     birthday = date(year=1993, month=11, day=9)
 
-    before_birthday = date(year=2017, month=11, day=8)
     now_birthday = date(year=2017, month=11, day=9)
+
+    before_birthday = date(year=2017, month=11, day=8)
     later_birthday = date(year=2017, month=11, day=10)
 
     before_month_birthday = date(year=2017, month=10, day=9)
-    now_month_birthday = date(year=2017, month=11, day=9)
     later_month_birthday = date(year=2017, month=12, day=9)
 
     before_year_birthday = date(year=2016, month=11, day=9)
-    now_year_birthday = date(year=2017, month=11, day=9)
     later_year_birthday = date(year=2018, month=11, day=9)
 
+    def test_calc_one_year(self):
+        res = calc_age_uptoday(date(2017, 5, 6), date(2018, 5, 6))
+        self.assertEqual(res[YEARS], 1)
+
+    def test_calc_age_uptoday_month(self):
+        res = calc_age_uptoday(date(2017, 11, 30), date(2017, 12, 29))
+        self.assertEqual(res[MONTHS], 0)
+
+        res = calc_age_uptoday(date(2017, 11, 30), date(2017, 12, 30))
+        self.assertEqual(res[MONTHS], 1)
+
+        res = calc_age_uptoday(date(2017, 11, 30), date(2017, 12, 31))
+        self.assertEqual(res[MONTHS], 1)
+
+        res = calc_age_uptoday(date(2017, 11, 30), date(2018, 1, 29))
+        self.assertEqual(res[MONTHS], 1)
+
+        res = calc_age_uptoday(date(2017, 11, 30), date(2018, 1, 30))
+        self.assertEqual(res[MONTHS], 2)
+
+        res = calc_age_uptoday(date(2017, 11, 30), date(2018, 2, 27))
+        self.assertEqual(res[MONTHS], 2)
+        #
+        # res = calc_age_uptoday(date(2017, 11, 30), date(2018, 2, 28))
+        # self.assertEqual(res[MONTHS], 3)
+
+        # res = calc_age_uptoday(date(2017, 11, 30), date(2018, 3, 1))
+        # self.assertEqual(res[MONTHS], 3)
+        #
+        # res = calc_age_uptoday(date(2017, 11, 30), date(2018, 3, 29))
+        # self.assertEqual(res[MONTHS], 3)
+        #
+        # res = calc_age_uptoday(date(2017, 11, 30), date(2018, 3, 30))
+        # self.assertEqual(res[MONTHS], 4)
+
+    def test_calc_age_uptoday_one_month_before(self):
+        res = calc_age_uptoday(date(2016, 11, 30), date(2017, 10, 29))
+        self.assertEqual(res[MONTHS], 10)
+
+        res = calc_age_uptoday(date(2016, 11, 30), date(2017, 10, 30))
+        self.assertEqual(res[MONTHS], 11)
+
+        res = calc_age_uptoday(date(2016, 11, 30), date(2017, 10, 31))
+        self.assertEqual(res[MONTHS], 11)
+
+        res = calc_age_uptoday(date(2016, 11, 30), date(2017, 11, 1))
+        self.assertEqual(res[MONTHS], 11)
+
+        res = calc_age_uptoday(date(2016, 11, 30), date(2017, 10, 29))
+        self.assertEqual(res[MONTHS], 11)
+
+        res = calc_age_uptoday(date(2016, 11, 30), date(2017, 11, 30))
+        self.assertEqual(res[MONTHS], 0)
+
+    def test_calc_one_month_later(self):
+        res = calc_age_uptoday(date(2017, 11, 30), date(2017, 12, 31))
+        self.assertEqual(res[MONTHS], 1)
+
+    def test_calc_one_day(self):
+        res = calc_age_uptoday(date(2017, 12, 30), date(2017, 12, 31))
+        self.assertEqual(res[DAYS], 1)
+
     def test_calc_years(self):
-        self.assertEqual(calc_age_uptoday(self.birthday, self.before_birthday)[YEARS], 23)
         self.assertEqual(calc_age_uptoday(self.birthday, self.now_birthday)[YEARS], 24)
-        self.assertEqual(calc_age_uptoday(self.birthday, self.later_birthday)[YEARS], 24)
+
         self.assertEqual(calc_age_uptoday(self.birthday, self.before_birthday)[YEARS], 23)
-        self.assertEqual(calc_age_uptoday(self.birthday, self.now_birthday)[YEARS], 24)
         self.assertEqual(calc_age_uptoday(self.birthday, self.later_birthday)[YEARS], 24)
-        self.assertEqual(calc_age_uptoday(self.birthday, self.before_birthday)[YEARS], 23)
-        self.assertEqual(calc_age_uptoday(self.birthday, self.now_birthday)[YEARS], 24)
-        self.assertEqual(calc_age_uptoday(self.birthday, self.later_birthday)[YEARS], 24)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_month_birthday)[YEARS], 23)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_month_birthday)[YEARS], 24)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_year_birthday)[YEARS], 23)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_year_birthday)[YEARS], 25)
+
+
+        
+    def test_calc_month(self):
+        self.assertEqual(calc_age_uptoday(self.birthday, self.now_birthday)[MONTHS], 0)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_birthday)[MONTHS], 11)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_birthday)[MONTHS], 0)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_month_birthday)[MONTHS], 10)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_month_birthday)[MONTHS], 1)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_year_birthday)[MONTHS], 0)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_year_birthday)[MONTHS], 0)
+
+    def test_calc_day(self):
+        self.assertEqual(calc_age_uptoday(self.birthday, self.now_birthday)[DAYS], 0)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_birthday)[DAYS], 30)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_birthday)[DAYS], 1)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_month_birthday)[DAYS], 0)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_month_birthday)[DAYS], 0)
+
+        self.assertEqual(calc_age_uptoday(self.birthday, self.before_year_birthday)[DAYS], 0)
+        self.assertEqual(calc_age_uptoday(self.birthday, self.later_year_birthday)[DAYS], 0)
 
     def test_calc_uptodate(self):
         birthday = self.birthday
@@ -58,24 +146,19 @@ class TestCalcAgeUpToDay(TestCase):
         m = 'month check - birthday: {b}, date: {d}'
         self.assertDictEqual(d1, d2, msg=m.format(b=birthday, d=now))
 
-        now = self.now_month_birthday
-        d1 = calc_age_uptoday(birthday, now)
-        d2 = {YEARS: 24, MONTHS: 0, DAYS: 0}
-        m = 'month check - birthday: {b}, date: {d}'
-        self.assertDictEqual(d1, d2, msg=m.format(b=birthday, d=now))
-
         now = self.later_month_birthday
         d1 = calc_age_uptoday(birthday, now)
         d2 = {YEARS: 24, MONTHS: 1, DAYS: 0}
         m = 'month check - birthday: {b}, date: {d}'
         self.assertDictEqual(d1, d2, msg=m.format(b=birthday, d=now))
 
-        # now = date(2017, 9, 7)
-        # birthday = date(2016, 10, 31)
-        # d1 = calc_age_uptoday(birthday, now)
-        # d2 = {YEARS: 24, MONTHS: 1, DAYS: 0}
-        # m = 'month check - birthday: {b}, date: {d}'
-        # self.assertDictEqual(d1, d2, msg=m.format(b=birthday, d=now))
+        #####################
+        now = date(2017, 9, 7)
+        birthday = date(2016, 10, 31)
+        d1 = calc_age_uptoday(birthday, now)
+        d2 = {YEARS: 0, MONTHS: 11, DAYS: 8}
+        m = 'month check - birthday: {b}, date: {d}'
+        self.assertDictEqual(d1, d2, msg=m.format(b=birthday, d=now))
 
         now = date(2017, 10, 30)
         birthday = date(2016, 10, 31)
