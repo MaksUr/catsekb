a98d1842e16729f8197be564a454097f0a04dbbf
MaksUr <maksim.urakv@gmail.com>
2017-09-19 16:42:37 +0500

    add last, next

--- cats/views.py
+++ cats/views.py
@@ -12,21 +12,34 @@ def index(request):
 
 
 def animal(request, animal_id, group_id=None):
-    next_animal_id = 5  # TODO: set next
-    last_animal_id = 3  # TODO: set next
-    animal_by_id = Animal.objects.get(id=animal_id, show=True)  # TODO: check does not exist
-    return render(
-        request,
-        'cats/animal.html',
-        {
-            'animal': animal_by_id, 'next': next_animal_id, 'last': last_animal_id
-        }
-    )
+    params = dict()
+    query = {'id': animal_id, 'show': True}
+    if group_id == 'all':
+        params['group'] = 'all'
+    elif isinstance(group_id, int):
+        params['group'] = group_id
+        query['group'] = Group.objects.get(show=True, id=group_id)  # TODO: check does not exist
+
+    animal_by_id = Animal.objects.get(**query)  # TODO: check does not exist
+    if group_id is not None:
+        # next_animal_id = animal_by_id.get_next_by_id().id  # TODO: set next
+        # last_animal_id = animal_by_id.get_previous_by_id().id  # TODO: set next
+        next_animal_id = 3
+        last_animal_id = 5
+        params['next'] = next_animal_id
+        params['last'] = last_animal_id
+    params['animal'] = animal_by_id
+    return render(request, 'cats/animal.html', params)
 
 
-def group(request, group_id):
-    group = Group.objects.get(id=group_id, show=True)  # TODO: check does not exist
-    name = group.name
-    id = group.id
-    animals = group.animal_set.filter(show=True)
+def group(request, group_id):  # TODO: rename
+    if group_id == 'all':
+        animals = Animal.objects.filter(show=True)
+        name = 'Все'
+        id = group_id
+    else:
+        group = Group.objects.get(id=group_id, show=True)  # TODO: check does not exist
+        name = group.name
+        id = group.id
+        animals = group.animal_set.filter(show=True)
     return render(request, 'cats/group.html', {'group': animals, 'name': name, 'id': id})
