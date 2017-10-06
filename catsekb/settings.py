806be3b0cfc30aa1a16bde12df5b8a28ed13bc3a
MaksUr <maksim.urakv@gmail.com>
2017-10-06 10:41:16 +0500

    add base site

--- catsekb/settings.py
+++ catsekb/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, "cats", "static", "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
