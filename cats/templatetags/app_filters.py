e82aba23357d3cb093fcfc340298b4d56310a55b
MaksUr <maksim.urakv@gmail.com>
2017-10-31 16:26:00 +0500

    add pagination to gallery

--- cats/templatetags/app_filters.py
+++ cats/templatetags/app_filters.py
@@ -7,3 +7,19 @@ def return_item_by_index(l, i):
         return l[i-1]
     except IndexError:
         return None
+
+
+@register.filter
+def change_page(query, page=None):
+    """
+
+    :type query: QueryDict
+    """
+    query._mutable = True
+    if page is not None:
+        query['page'] = page
+    try:
+        res = query.urlencode()
+    except AttributeError:
+        res = ''
+    return res
