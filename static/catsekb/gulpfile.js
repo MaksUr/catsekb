af3f7657defc2a47218f878419a6ae9cbb9186f6
MaximKisseloff <maxoonix@gmail.com>
2017-11-18 19:38:41 +0500

    Починил gulpfile.js

--- static/catsekb/gulpfile.js
+++ static/catsekb/gulpfile.js
@@ -1,18 +1,16 @@
-var gulp 		 = require('gulp'), // Подключаем Gulp
-    sass 		 = require('gulp-sass'); // Подключаем Sass пакет
-    autoprefixer = require('gulp-autoprefixer'); // Подключаем библиотеку для автоматического добавления префиксов
+var gulp       = require('gulp'), // Подключаем Gulp
+    sass         = require('gulp-sass'), //Подключаем Sass пакет,
+    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
     cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename       = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
-    del          = require('del'); // Подключаем библиотеку для удаления файлов и папок
+    rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
-gulp.task('sass', function() { // Создаем таск "sass"
-  return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss']) // Берем источник
-    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-  });
-
-gulp.task('clean', function() {
-    return del.sync('dist'); // Удаляем папку dist перед сборкой
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
 
 gulp.task('css-libs', ['sass'], function() {
@@ -22,16 +20,18 @@ gulp.task('css-libs', ['sass'], function() {
         .pipe(gulp.dest('app/css/min')); // Выгружаем в папку app/css
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['app/sass/**/*.sass', 'app/sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
+gulp.task('watch', ['css-libs'], function() {
+    gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
 });
 
-gulp.task('default', ['watch']);
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
 
 gulp.task('build', ['clean', 'css-libs'], function() {
 
     var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-        'app/css/bootstrap/bootstrap.min.css',
+        'app/css/bootstrap/*.min.css',
         'app/css/min/*.css'
         ])
     .pipe(gulp.dest('dist/css'))
@@ -42,7 +42,9 @@ gulp.task('build', ['clean', 'css-libs'], function() {
     var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
     .pipe(gulp.dest('dist/js'))
 
-    var buildImg = gulp.src('app/img/**/*') // Переносим img в продакшен
-    .pipe(gulp.dest('dist/img'));
+    var buildImg = gulp.src('app/img/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/img'))
 
 });
+
+gulp.task('default', ['watch']);
