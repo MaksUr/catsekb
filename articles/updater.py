65d745f3d744c6c9e84e3cf9c308ac3f86818d31
MaksUr <maksim.urakv@gmail.com>
2017-12-13 21:18:10 +0500

    Добавление отдельных постов

--- articles/updater.py
+++ articles/updater.py
@@ -22,3 +22,19 @@ def get_wall_news(group_id=VK_GROUP_ID):
     ).json()
     save_json(join('articles', 'articles_list', 'news.json'), news)
     return news
+
+
+def get_post_by_id_list(posts_ids, group_id=VK_GROUP_ID):
+    group_id = -1 * group_id
+    s = str(group_id) + ','
+    posts = s.join(posts_ids)
+    access_token = input('token <<')
+    news = requests.get(
+        r'https://api.vk.com/method/wall.get', params={
+            'posts': posts,
+            'access_token': access_token
+        }
+    ).json()
+    save_json(join('articles', 'articles_list', 'news2.json'), news)
+    return news
+
