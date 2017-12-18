5f8f50a6d7a4e25b34b273f3699fb4ec21f7fd4d
MaksUr <maksim.urakv@gmail.com>
2017-12-18 13:44:58 +0500

    работа с index_view

--- catsekb/views.py
+++ catsekb/views.py
@@ -6,21 +6,36 @@ from cats.views import get_animals_from_query, GALLERY_DEFAULT_ITEMS_COUNT
 from catsekb.constants import URL_NAME_INDEX_TITLE, INDEX
 from catsekb.view_functions import get_base_context, get_important_news
 
+HOME_ANIMALS_COUNT = 0
 
-def index_view(request):
-    show_permission = request.user.is_authenticated()
-    context = get_base_context(show_permission=show_permission, active_menu=INDEX, extra_title=URL_NAME_INDEX_TITLE)
+
+def get_shelter_animals(show_permission, count=GALLERY_DEFAULT_ITEMS_COUNT):
     query = {ANIMAL_LOCATION_STATUS: ANIMAL_LOCATION_STATUS_SHELTER}
     shelter_animals = get_animals_from_query(
         query=query, show_permission=show_permission
     ).order_by('?')
-    context['shelter_animals'] = shelter_animals[:GALLERY_DEFAULT_ITEMS_COUNT]
-    context['shelter_caption'] = GROUP_INSTANCE_SHELTER_NAME
-    context['shelter_animals_count'] = shelter_animals.count()
-    context['important_news'] = get_important_news()
-    context['home_animals_count'] = get_animals_from_query(
+    return shelter_animals[:count], len(shelter_animals)
+
+
+def get_home_animals_count():
+    global HOME_ANIMALS_COUNT
+    get_animals_from_query(
         query={ANIMAL_LOCATION_STATUS: ANIMAL_LOCATION_STATUS_HOME}, show_permission=True
-    ).count()  # TODO: need correct number
+    ).count()
+    return HOME_ANIMALS_COUNT or get_animals_from_query(
+        query={ANIMAL_LOCATION_STATUS: ANIMAL_LOCATION_STATUS_HOME}, show_permission=True
+    ).count()
+
+
+def index_view(request):
+    show_permission = request.user.is_authenticated()
+    context = get_base_context(show_permission=show_permission, active_menu=INDEX, extra_title=URL_NAME_INDEX_TITLE)
+    shelter_animals, shelter_animals_count = get_shelter_animals(show_permission=show_permission)
+    context['shelter_animals'] = shelter_animals
+    context['shelter_caption'] = GROUP_INSTANCE_SHELTER_NAME
+    context['shelter_animals_count'] = shelter_animals_count
+    context['important_news'] = get_important_news()
+    context['home_animals_count'] = get_home_animals_count()
     context['animals_count'] = get_animals_from_query(query=dict(), show_permission=True).count()
     if show_permission is True:
         context['dying_animals_count'] = get_animals_from_query(
