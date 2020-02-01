538ccbe830d351bcf7a1ba13c40af34368c87b47
Максим Ураков <maksim.urakv@gmail.com>
2020-02-01 13:23:55 +0500

    Husky page editing

--- huskyekb_page/views.py
+++ huskyekb_page/views.py
@@ -6,4 +6,8 @@ from catsekb.view_functions import get_shelter_animals
 def huskyekb_page_view(request):
     show_permission = request.user.is_authenticated
     shelter_animals, shelter_animals_count = get_shelter_animals(show_permission=show_permission, project='huskyekb')
-    return render(request, 'huskyekb_page/index.html', {'shelter_animals': shelter_animals})
+    context = {
+        'shelter_animals': shelter_animals,
+        'shelter_animals_count': shelter_animals_count
+    }
+    return render(request, 'huskyekb_page/index.html', context)
